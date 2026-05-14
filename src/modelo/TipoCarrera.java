a11289054b217aebb2a5b6c80f16099a0411fa79
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-05-14 17:27:29 -0600

    Agregar clase Curso con inscripcion de estudiantes y uso de ArrayList

---
+++ src/modelo/TipoCarrera.java
@@ -0,0 +1,16 @@
+package modelo;
+
+// enum que representa los tipos de carrera disponibles en la universidad
+public enum TipoCarrera {
+
+    // carrera de ingenieria en sistemas
+    INGENIERIA_SISTEMAS,
+    // carrera de administracion de empresas
+    ADMINISTRACION_EMPRESAS,
+    // carrera de derecho
+    DERECHO,
+    // carrera de medicina
+    MEDICINA,
+    // carrera de psicologia
+    PSICOLOGIA
+}
