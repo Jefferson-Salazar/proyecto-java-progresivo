067b79036a8a51abd7c6d2affd7a4337b4710136
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-05-14 17:24:10 -0600

    Actualizar Main para probar Curso, TipoCarrera y sobrecarga de metodos

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,47 @@
 package main;
 
-// importar la clase Persona para usarla aqui
+// importar las clases necesarias para la prueba
+import modelo.Curso;
 import modelo.Persona;
+import modelo.TipoCarrera;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
         // crear un objeto de tipo Persona para probar
         Persona persona1 = new Persona("Carlos", 20, "001");
-        
+
         // mostrar la informacion de la persona en pantalla
         persona1.mostrarInfo();
-        
+
         // cambiar el nombre usando el setter
         persona1.setNombre("Carlos Ramirez");
-        
+
         // mostrar el nombre actualizado usando el getter
         System.out.println("Nombre actualizado: " + persona1.getNombre());
+
+        System.out.println("----------------------------");
+
+        // crear un curso usando el enum TipoCarrera
+        Curso curso1 = new Curso("Programacion 1", "PRG101", 4, TipoCarrera.INGENIERIA_SISTEMAS);
+
+        // mostrar la informacion del curso
+        curso1.mostrarInfo();
+
+        // inscribir estudiantes al curso
+        curso1.inscribirEstudiante("2024001");
+        curso1.inscribirEstudiante("2024002");
+
+        // intentar inscribir un estudiante que ya existe
+        curso1.inscribirEstudiante("2024001");
+
+        // inscribir con mensaje personalizado usando overloading
+        curso1.inscribirEstudiante("2024003", "Bienvenido al curso de Programacion 1");
+
+        System.out.println("----------------------------");
+
+        // mostrar la informacion actualizada del curso
+        curso1.mostrarInfo();
     }
 }
